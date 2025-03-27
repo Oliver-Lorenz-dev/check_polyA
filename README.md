63c037315988ffe06622df89554404b3993a8242
Oliver-Lorenz-dev <oliverlorenz.business@gmail.com>
2025-03-27 14:59:13 +0000

    add scripts and data

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # check_polyA
+Python script which checks for polyA monomers 100bp upstream of CDS regions (AAAAAAAA)
+
+## Description
+This script checks sequences for polyA monomers 100bp upstream of CDS regions (AAAAAAAA)
+
+The script will output a file called `results.csv` which contains the polyA count for each isolate in `data.zip`
+
+The `results.csv` file follows the below format:
+```csv
+isolate,polya_count
+4954-98,33
+```
+
+## Setup
+Clone the repository and install the dependencies:
+```shell
+git clone https://github.com/Oliver-Lorenz-dev/check_polyA.git
+cd check_polyA
+python3 -m venv venv
+source venv/bin/activate
+pip install bio
+```
+
+## Run the script on the dataset
+```shell
+./run.sh
+```
+
+## Run the python script on individual samples
+```shell
+./check_polya.py <filename.fasta> <filename.gbk>
+```
---
+++ check_polya.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+from Bio import SeqIO
+from Bio.Seq import Seq
+
+polya_count = 0
+fasta_file = sys.argv[1]
+genbank_file = sys.argv[2]
+
+
+def read_fasta(fasta_file: str, contig_num: int) -> str:
+    i = 1
+    if contig_num == 1:
+        with open(fasta_file) as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                return record.seq
+    else:
+        with open(fasta_file) as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                if i == contig_num:
+                    return record.seq
+                i += 1
+
+
+def read_genbank(genbank_file: str, contig_num: int) -> list:
+    i = 1
+    if contig_num == 1:
+        for record in SeqIO.parse(genbank_file, "genbank"):
+            features = record.features
+            return features
+    else:
+        for record in SeqIO.parse(genbank_file, "genbank"):
+            if i == contig_num:
+                features = record.features
+                return features
+            i += 1
+
+def get_polya_count(feature) -> int:
+    polya_count = 0
+    match_pattern = r'A{8,}'
+    start = int(feature.location.start)
+    end = int(feature.location.end)
+    if start >= 100:
+        if feature.location.strand == 1:
+            upstream_sequence = sequence[(start - 100): (start)]
+        elif feature.location.strand == -1:
+            upstream_sequence = sequence[(start - 100): (start)].reverse_complement()
+        polya_count = len(re.findall(match_pattern, str(upstream_sequence)))
+    return polya_count
+
+for i in range(1, (sum(1 for _ in SeqIO.parse(fasta_file, "fasta")) + 1)):
+    sequence = read_fasta(fasta_file, i)
+    gb_features = read_genbank(genbank_file, i)
+    for feature in gb_features:
+        if feature.type == "CDS":
+            polya_count += get_polya_count(feature)  
+
+print(polya_count)       
